14b5393ff8c0d09fa74adff61f59075c89f9b80c
Craig Killian <ckillian5668@gmail.com>
2017-04-30 20:55:51 -0500

    Added basic express config

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-function test (){
+var express = require('express');
+var app = express();
+var mongo = require('mongodb');
+var bodyParser = require('body-parser');
 
-	var x;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('build'));
+app.set('views', './build');
 
-}
-ddd
-
-dfsdf
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
