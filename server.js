58423bd630b9068912b59439fbb46efa98dfb516
Craig Killian <ckillian5668@gmail.com>
2017-04-30 22:24:05 -0500

    Added Stuff

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var express = require('express');
+
+// Modules
+var express = require(`express`);
+var path = require(`path`);
+
+// Express Port/App Declaration
+var PORT = process.env.PORT || 3000;
 var app = express();
-var mongo = require('mongodb');
-var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static('build'));
-app.set('views', './build');
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Routes
+app.get(`*`, function(req, res) {
+  res.sendFile('public/index.html', { root: __dirname });
+});
+
+// Connection to PORT
+app.listen(PORT, function() {
+  console.log(`Listening On Port: ${PORT}`);
 });
